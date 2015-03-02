4084d77aab3f5d051fff1665012689acf6ade615
Aleksey Filippov <sarum9in@gmail.com>
2015-03-02 02:15:58 +0100

    Improve gRPC lookup

--- scripts/grpc.cmake
+++ scripts/grpc.cmake
@@ -1,10 +1,14 @@
 find_program(GRPC_CPP_PLUGIN_EXECUTABLE NAMES grpc_cpp_plugin)
-find_library(GRPC_LIBRARY NAMES grpc)
-find_library(GRPCXX_LIBRARY NAMES grpc++)
+find_path(GRPC_INCLUDE_DIRS NAMES grpc/grpc.h)
+find_path(GRPCXX_INCLUDE_DIRS NAMES grpc++/channel_interface.h)
+find_library(GRPC_LIBRARIES NAMES grpc)
+find_library(GRPCXX_LIBRARIES NAMES grpc++)
 
 message("Found grpc plugin at ${GRPC_CPP_PLUGIN_EXECUTABLE}")
-message("Found grpc library at ${GRPC_LIBRARY}")
-message("Found grpc++ library at ${GRPCXX_LIBRARY}")
+message("Found grpc headers at ${GRPC_INCLUDE_DIRS}")
+message("Found grpc library at ${GRPC_LIBRARIES}")
+message("Found grpc++ headers at ${GRPCXX_INCLUDE_DIRS}")
+message("Found grpc++ library at ${GRPCXX_LIBRARIES}")
 
 macro(bunsan_add_rpc_cxx_library)
     bunsan_add_protobuf_cxx_library(
